db1e874ccda0f631e39f5517899519ac22c4a643
7assanosama <7assanosama@gmail.com>
2026-04-17 15:01:14 +0200

    ready to production

--- bot.py
+++ bot.py
@@ -84,7 +84,7 @@ if __name__ == "__main__":
 
     application.add_handler(MessageHandler(filters.Regex("^(🌐 تغيير اللغة|🌐 Language)$"), change_lang))
     application.add_handler(MessageHandler(filters.Regex("^(🇸🇦 العربية|🇬🇧 English)$"), set_lang))
-    application.add_handler(MessageHandler(filters.Regex("^(ℹ️ مساعدة|ℹ️ Help)$"), help_command))
+    application.add_handler(MessageHandler(filters.Regex("^(ℹ️ الدعم|ℹ️ support)$"), help_command))
     application.add_handler(MessageHandler(filters.Regex("^(💎 خطط التسعير|💎 Pricing Plans)$"), plans_command))
 
     conv = ConversationHandler(
